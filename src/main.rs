38c4cc5a20606f1d4ac3d9c4adc4bf40f1554528
Kyle Clemens <git@kyleclemens.com>
2018-08-11 12:14:01 -0400

    fix: exit with non-zero exit code on failure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(process_exitcode_placeholder)]
+
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
@@ -9,6 +11,8 @@ mod logging;
 use crate::list::WordList;
 
 use rand::thread_rng;
+
+use std::process::ExitCode;
 
 const WORD_LIST_JSON: &'static str = include_str!(concat!(env!("OUT_DIR"), "/json"));
 
@@ -22,7 +26,7 @@ fn word_list(name: &str) -> Option<&WordList> {
   WORD_LISTS.iter().find(|x| x.short_names.contains(&name))
 }
 
-fn main() {
+fn main() -> ExitCode {
   let args = crate::cli::app(&*WORD_LISTS).get_matches();
 
   let list_name = args.value_of("list").expect("list value missing");
@@ -30,19 +34,19 @@ fn main() {
   let num_words: u8 = match args.value_of("words").expect("words value missing").parse() {
     Ok(x) if x == 0 => {
       eprintln!("must use at least one word");
-      return;
+      return ExitCode::FAILURE;
     },
     Ok(x) => x,
     Err(e) => {
       eprintln!("invalid amount of words: {}", e);
-      return;
+      return ExitCode::FAILURE;
     },
   };
   let num_pws: usize = match args.value_of("passphrases").expect("passphrases value missing").parse() {
     Ok(x) => x,
     Err(e) => {
       eprintln!("invalid amount of passphrases: {}", e);
-      return;
+      return ExitCode::FAILURE;
     },
   };
 
@@ -64,4 +68,6 @@ fn main() {
       println!("{}", passphrase);
     }
   }
+
+  ExitCode::SUCCESS
 }
